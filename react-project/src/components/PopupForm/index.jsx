456903e93b97c5a0fd625a285e54f41ad42aadd8
Alisa Chentsova <minchina.alice@gmail.com>
2023-08-11 11:14:26 +0400

    form submit fncs

--- react-project/src/components/PopupForm/index.jsx
+++ react-project/src/components/PopupForm/index.jsx
@@ -1,31 +1,47 @@
-import React from "react";
 import './styles.css';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 const PopupForm = () => {
-    // const [isOpen, setIsOpen] = useState('');
+    const [getValue, setGetValue] = useState('');
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setGetValue(values => ({ ...values, [value]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('Введено новое слово: ' + getValue);
+    }
+
+    // const saveNewWord = (event) => setGetValue({ value: event.target.value });
+    // console.log('Введено новое слово: ' + getValue);
+
     return (
-        <div className="form">
-            <form action="">
-                <label for="eng">Введите слово на английском</label>
-                <input id="eng" name="new-word-eng" type="text" />
+        <>
+            <div className="form">
+                <form action="" onSubmit={handleSubmit}>
+                    <label for="eng-word">Введите слово на английском языке</label>
+                    <input id="eng-word" onChange={handleChange} value={getValue.newEngWord} name="newEngWord" type="text" placeholder="Введите слово" required autocomplete="new-eng-word" />
 
-                <label for="rus">Введите перевод слова на русском</label>
-                <input id="rus" name="new-word-rus" type="text" />
+                    <label for="rus-word">Введите перевод слова на русском языке</label>
+                    <input id="rus-word" onChange={handleChange} value={getValue.newRusWord} name="newRusWord" type="text" placeholder="Введите перевод" required autocomplete="new-rus-word" />
 
-                <label for="tag">Выберите тег к слову</label>
-                <select id="tag">
-                    <option selected value="chooseTag"></option>
-                    <option value="domesticAnimals">Домашние животные</option>
-                    <option value="food">Еда</option>
-                    <option value="home">Дом</option>
-                    <option value="nature">Природа</option>
-                    <option value="transport">Транспорт</option>
-                </select>
+                    <label for="word-tag">Выберите подходящий к слову тег</label>
+                    <select id="word-tag">
+                        <option selected value="chooseTag">Выберите тег из списка</option>
+                        <option value="domesticAnimals">Домашние животные</option>
+                        <option value="food">Еда</option>
+                        <option value="home">Дом</option>
+                        <option value="nature">Природа</option>
+                        <option value="transport">Транспорт</option>
+                    </select>
 
-                <button className="btn-save">Сохранить</button>
-            </form>
-        </div>
+                    <button onClick={handleSubmit} className="btn-save">Сохранить</button>
+                </form>
+            </div>
+        </>
     )
 }
 
